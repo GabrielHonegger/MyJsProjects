39a7a0f9cd276b20e7d15406cf8f06e3357608b8
GabrielHonegger <gabrielhonegger132@gmail.com>
2023-10-18 21:26:02 +0200

    saving

---
+++ js_projects/Find Pi/main.js
@@ -0,0 +1,11 @@
+let perimeter = prompt("perimeter (cm): ");
+let diameter = prompt("diameter (cm): ");
+
+if ((perimeter / diameter) == 3.14) {
+    alert("Isso é um círculo!");
+}
+
+else {
+    alert("Isso é outra figura!");
+}
+
