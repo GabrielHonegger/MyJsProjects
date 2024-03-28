3a8b1ca87fd7fddf46d9631fce19a5c64261015d
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-03-28 18:30:37 -0300

    new netflix

---
+++ netflix/script.js
@@ -0,0 +1,15 @@
+function activateFocus() {
+    document.getElementById("input-field").focus();
+}
+function activateFocus2() {
+    document.getElementById("input-field2").focus();
+}
+function changePageLanguage() {
+    let pageLang = window.location.href;
+    if (pageLang.includes('/netflix-landing-page/pt-BR/index.html') || window.location.href == "") {
+        window.location.href = "/netflix-landing-page/en/index.html";
+    }
+    else {
+        window.location.href = "/netflix-landing-page/pt-BR/index.html";
+    }
+}
